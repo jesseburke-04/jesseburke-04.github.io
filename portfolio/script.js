a5098eee09e24f67c174624194febbd192204b99
jesseburke-04 <jesseburke04@gmail.com>
2026-04-09 21:42:43 -0400

    changes

--- portfolio/script.js
+++ portfolio/script.js
@@ -10,3 +10,11 @@ document.querySelector(".digital-takeover-btn").addEventListener("click", functi
     }
 });
 
+document.querySelector(".contact-form").addEventListener("submit", function(event) {
+    // make sure the page doesn't refresh
+    event.preventDefault();
+    // use a variable to store users input
+    let name = document.querySelector("#name").value;
+    // change the text and call the users input
+    document.querySelector(".liveOutput").textContent = "Welcome, " + name + "!";
+});
