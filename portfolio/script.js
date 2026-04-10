8744cfeb9acf415b46ece50636d58db4cdb15dca
jesseburke-04 <jesseburke04@gmail.com>
2026-04-09 23:12:23 -0400

    changes

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,8 +1,9 @@
-
+//This is my JavaScript file for my portfolio, the reflection is in my CSS Stylesheet
+// Ceating the event for my color changing button, and making sure you can toggle it on and off.
 document.querySelector(".digital-takeover-btn").addEventListener("click", function() {
-    // finds the digital-takeover class and toggles it on or off
+    // Finds the digital-takeover class and toggles it on or off
     document.body.classList.toggle("digital-takeover");
-    // using an if statement to change the link color from pink to black
+    // Using an if statement to change the link color from pink to black
     if (document.body.classList.contains("digital-takeover")) {
         document.documentElement.style.setProperty('--link-color', '#D71AE6');
     } else {
@@ -10,20 +11,22 @@ document.querySelector(".digital-takeover-btn").addEventListener("click", functi
     }
 });
 
+// Creating the even for the name button, in my contact form.
 document.querySelector(".name-btn").addEventListener("click", function(event) {
-    // make sure the page doesn't refresh
+    // Make sure the page doesn't refresh
     event.preventDefault();
     // use a variable to store users input
     let name = document.querySelector(".name").value;
-    // change the text and call the users input
+    // Change the text and call the users input
     document.querySelector(".name-output").textContent = "Welcome, " + name + "!";
 });
 
+// Creating the even for my email button, and how the email box will disappear.
 document.querySelector(".email-btn").addEventListener("click", function(event) {
-    // make sure the page doesn't refresh
+    // Make sure the page doesn't refresh
     event.preventDefault();
-    // hide the email box
+    // Hide the email box
     document.querySelector(".email").parentElement.style.display = "none";
-    // show the email submitted message
+    // Show the email submitted message
     document.querySelector(".email-output").textContent = "Email has been submitted!";
 });
