486ad8d128b52d6fc4a78c8ffd43a0973cd9438c
jesseburke-04 <jesseburke04@gmail.com>
2026-04-09 22:50:28 -0400

    changes

--- portfolio/script.js
+++ portfolio/script.js
@@ -4,9 +4,9 @@ document.querySelector(".digital-takeover-btn").addEventListener("click", functi
     document.body.classList.toggle("digital-takeover");
     // using an if statement to change the link color from pink to black
     if (document.body.classList.contains("digital-takeover")) {
-        document.documentElement.style.setProperty('--link-color', 'black');
-    } else {
         document.documentElement.style.setProperty('--link-color', '#D71AE6');
+    } else {
+        document.documentElement.style.setProperty('--link-color', '#1a1c20');
     }
 });
 
